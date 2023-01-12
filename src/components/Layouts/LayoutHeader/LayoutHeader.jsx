284564cc7a73d1fbe17ba3794da8df2fa22ed8e6
mohamedy72 <mohamedwebdev94@gmail.com>
2023-01-12 16:55:34 +0200

    feat: Header is fully responsive

--- src/components/Layouts/LayoutHeader/LayoutHeader.jsx
+++ src/components/Layouts/LayoutHeader/LayoutHeader.jsx
@@ -1,5 +1,5 @@
 import "./layoutheader.css";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, Fragment } from "react";
 import Button from "@Components/UI/Button/Button";
 import { Dropdown } from "@Components/UI/Widgets";
 import useBodyDismiss from "@Hooks/useBodyDismiss";
@@ -26,6 +26,7 @@ const LayoutHeader = ({
   useEffect(() => {
     useBodyDismiss(hRef, setHeaderDropdown);
   }, [hRef]);
+
   return (
     <section className="layoutheader">
       <div className="layout_text">
@@ -33,14 +34,35 @@ const LayoutHeader = ({
         <p>{paragraph}</p>
       </div>
       {hasDropdown ? (
-        <Button
-          btnClass={btnClass}
-          iconDir={iconDir}
-          label={label}
-          icon={icon}
-          handleHeaderDropdown={handleHeaderDropdown}
-          path={path}
-        />
+        <div className="layoutheader_cta">
+          <Button
+            btnClass={btnClass}
+            iconDir={iconDir}
+            label={label}
+            icon={icon}
+            handleHeaderDropdown={handleHeaderDropdown}
+            path={path}
+          />
+          <div className="dropdown" ref={hRef}>
+            {headerDropdown && (
+              <Dropdown>
+                <p>
+                  <FileEarmarkExcel />
+                  <a href="@Assets/files/sampleExcel.xls" download>
+                    Excel
+                  </a>
+                </p>
+                <hr />
+                <p>
+                  <FileEarmarkPdf />
+                  <a href="../../../assets/files/PDFSample.pdf" download>
+                    PDF
+                  </a>
+                </p>
+              </Dropdown>
+            )}
+          </div>
+        </div>
       ) : (
         <Button
           btnClass={btnClass}
@@ -50,25 +72,6 @@ const LayoutHeader = ({
           path={path}
         />
       )}
-      <div className="dropdown" ref={hRef}>
-        {headerDropdown && (
-          <Dropdown>
-            <p>
-              <FileEarmarkExcel />
-              <a href="@Assets/files/sampleExcel.xls" download>
-                Excel
-              </a>
-            </p>
-            <hr />
-            <p>
-              <FileEarmarkPdf />
-              <a href="../../../assets/files/PDFSample.pdf" download>
-                PDF
-              </a>
-            </p>
-          </Dropdown>
-        )}
-      </div>
     </section>
   );
 };

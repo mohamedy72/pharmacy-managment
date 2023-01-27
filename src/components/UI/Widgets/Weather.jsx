fe5511f8f9a77983623cd8b10c59a1386c8ab7c2
mohamedy72 <mohamedwebdev94@gmail.com>
2023-01-27 14:37:03 +0200

    fix: Render correct part of the day depending on the time

--- src/components/UI/Widgets/Weather.jsx
+++ src/components/UI/Widgets/Weather.jsx
@@ -8,6 +8,8 @@ import { BrightnessHighFill, MoonFill } from "react-bootstrap-icons";
 
 const Weather = ({ greet }) => {
   const [time, setTime] = useState(() => currentTime());
+  const hoursOfTheTime = +time.split("")[0];
+  const amOrPm = time.split(" ")[1];
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -23,9 +25,15 @@ const Weather = ({ greet }) => {
     <div className="weather">
       <p className="weather_header">
         <span className="weather_icon">
-          {time.includes("PM") ? <BrightnessHighFill /> : <MoonFill />}
+          {time.includes("PM") ? (
+            <BrightnessHighFill fill="#FAC05E" />
+          ) : (
+            <MoonFill />
+          )}
         </span>
-        {greet}
+        <span>
+          <RenderCorrectPartOfADay hours={hoursOfTheTime} timePeriod={amOrPm} />
+        </span>
       </p>
       <div className="weather_cal">
         <p className="weather_date">
@@ -49,3 +57,17 @@ const Weather = ({ greet }) => {
 };
 
 export default Weather;
+
+const RenderCorrectPartOfADay = ({ hours, timePeriod }) => {
+  if (timePeriod === "AM" && hours >= 5 && hours <= 12) {
+    return "Good Morning";
+  } else if (timePeriod === "PM" && hours <= 12 && hours <= 6) {
+    return "Good Afternoon";
+  } else if (timePeriod === "PM" && hours > 6 && hours <= 9) {
+    return "Good Evening";
+  } else if (timePeriod === "AM" || timePeriod === "PM") {
+    if (hours > 9 || (hours < 9 && hours <= 5)) {
+      return "Good Night";
+    }
+  }
+};
